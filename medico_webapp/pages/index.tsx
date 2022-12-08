0bb078be32cfdc6aa505b1d0a4825d7b2f562f8f
thedudeontitan <97180942+thedudeontitan@users.noreply.github.com>
2022-12-08 20:48:41 +0530

    solved window reference error

--- medico_webapp/pages/index.tsx
+++ medico_webapp/pages/index.tsx
@@ -1,20 +1,13 @@
 import Login from "../src/login"
 import Home from "../src/home"
-import Navbar from "../src/navigation/navbar"
-import Navbarlg from "../src/navigation/navbarlg";
+// import Navbar from "../src/navigation/navbar"
+// import Navbarlg from "../src/navigation/navbarlg";
+import React,{useEffect,useState} from "react";
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(()=>import("../src/navigation/navbar"),{ssr:false})
 
 export default function App() {
-  let width = window.innerWidth;
-     if (width > 768){
-  return (
-      <div  id="container">
-        {/* <Login/> */}
-        {/* <Home/> */}
-        
-        <Navbarlg/>
-      </div>
-  )}
-  else{
     return (
       <div  id="container">
         {/* <Login/> */}
@@ -22,5 +15,5 @@ export default function App() {
         
         <Navbar/>
       </div>
-  )}
+  )
 }
--- medico_webapp/src/navigation/navbar.tsx
+++ medico_webapp/src/navigation/navbar.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default function Navbar(){
+    let width = window.innerWidth;
+    if(width > 768){
+        return(
+            <section>
+            <nav className="fixed top-0 left-0 right-0 bg-blue-400 h-14 z-10">
+                <div className="flex">
+                    <ul className="flex flex-row">
+                    </ul>
+                </div>
+            </nav>
+        </section>
+        )
+    }
+    else{
     return(
         <section>
             <nav className="fixed bottom-0 left-0 right-0 bg-blue-400 h-14 z-10">
@@ -11,4 +25,6 @@ export default function Navbar(){
             </nav>
         </section>
     )
+    }
+
 }
